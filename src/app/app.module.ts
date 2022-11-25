bee4603d852c7abb5cef71173d53835e8daf39b9
Bhagyashree2000 <bhagyashreenemade2000@gmail.com>
2022-11-25 18:59:37 +0530

    login page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,12 +16,14 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatCardModule} from '@angular/material/card';
 import {MatSelectModule} from '@angular/material/select';
 import {MatExpansionModule} from '@angular/material/expansion';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
