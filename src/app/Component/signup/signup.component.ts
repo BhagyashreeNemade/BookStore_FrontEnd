fae0c4f3da0623fc14fcd8a533c5349232b9399a
Bhagyashree2000 <bhagyashreenemade2000@gmail.com>
2022-11-26 15:31:00 +0530

    error message applied to login and signup page

--- src/app/Component/signup/signup.component.ts
+++ src/app/Component/signup/signup.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
-  hide = true;
+export class SignupComponent implements OnInit {
+  RegisterForm! : FormGroup;
+  hide=true;
+
+  constructor( private formBuilder : FormBuilder) { }
+
+  ngOnInit(): void {
+    this.RegisterForm = this.formBuilder.group({
+      FullName: ['', Validators.required],
+      EmailId: ['', Validators.required],
+      Password: ['', Validators.required],
+      Mobile: ['', Validators.required]
+    })
+  }
 }
